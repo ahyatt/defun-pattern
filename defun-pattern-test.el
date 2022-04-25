b4226872a4b2c18010180e114b04db470d322477
Andrew Hyatt <ahyatt@gmail.com>
2022-04-24 20:38:15 -0400

    Change to use an arglist.

--- defun-pattern-test.el
+++ defun-pattern-test.el
@@ -2,7 +2,7 @@
 (require 'ert)
 
 (ert-deftest defun-pattern-test-constants-and-variables ()
-  (defun-pattern fibonacci
+  (defun-pattern fibonacci (n)
     "Compute the fibonacci sequence."
     ((0) 0)
     ((1) 1)
@@ -16,7 +16,7 @@
   (should (= (fibonacci 8) 21)))
 
 (ert-deftest defun-pattern-test-mixed ()
-  (defun-pattern crit
+  (defun-pattern crit (roll attributes)
     "D&D 5e critical hit computation, taking in the results of a d20
 and a list of attributes currently in effect."
     ((20 _) t)
@@ -27,7 +27,7 @@ and a list of attributes currently in effect."
   (should (crit 20 nil)))
 
 (ert-deftest defun-pattern-test-repeated ()
-  (defun-pattern repeatedp
+  (defun-pattern repeatedp (&rest _)
     "Test for repeated pattern, returns nil, 'once, or 'twice, or
 'split."
     ((a a) 'once)
@@ -42,10 +42,23 @@ and a list of attributes currently in effect."
   (should (eq 'split (repeatedp 3 'z 3))))
 
 (ert-deftest defun-pattern-test-destruct ()
-  (defun-pattern middle-eq-plus-one
+  (defun-pattern middle-eq-plus-one (&rest _)
     "Test for destructuring patterns."
     (((_ a _) b) (= (+ 1 a) b)))
   (should-not (middle-eq-plus-one '(3) 3))
   (should-not (middle-eq-plus-one '(z) 'z))
   (should-not (middle-eq-plus-one '(1 3 9) 3))
   (should (middle-eq-plus-one '(1 3 9) 4)))
+
+(ert-deftest defun-pattern-ordinary-arglist ()
+  (defun-pattern fibonacci-with-rest-args (&rest args)
+    "Compute the fibonacci sequence."
+    (((0)) 0)
+    (((1)) 1)
+    (((n))
+     (+ (fibonacci-with-rest-args (- n 1))
+        (fibonacci-with-rest-args (- n 2)))))
+  (should (= (fibonacci-with-rest-args 8) 21)))
+
+(ert-deftest defun-pattern-arglist-required ()
+  (should-error (defun-pattern my-bad-defun () (+ 1 1))))
--- defun-pattern.el
+++ defun-pattern.el
@@ -15,6 +15,7 @@
 ;;; Code:
 
 (require 'pcase)
+(require 'cl-extra)
 
 (defun defun-pattern-unquote (elem)
   "If ELEM is a inserted element, return uninserted version of ELEM.
@@ -54,20 +55,32 @@ Argument PATTERN is a pattern found in an `defun-pattern' arglist."
 (defun defun-pattern-to-pcase (pattern)
   "Transform a `defun-pattern' matcher to a pcase matcher.
 Argument PATTERN is an arglist that needs to be transformed."
-  (list '\` (defun-pattern-correct-quote pattern)))
+  (pcase pattern
+    ((pred listp) (list '\` (defun-pattern-correct-quote pattern)))
+    (_ pattern)))
+
+(defun defun-pattern-normal-args (arglist)
+  "Return all pre-&rest args in arglist."
+  (cl-subseq arglist 0 (cl-position '&rest arglist)))
+
+(defun defun-pattern-rest-arg (arglist)
+  "Return the &rest arg in arglist, as a 1-element list."
+  (when (eq (nth (- (length arglist) 2) arglist) '&rest)
+    (last arglist)))
 
 (def-edebug-elem-spec 'defun-pattern-patterns
   '((sexp def-body)))
 
 (def-edebug-spec defun-pattern
-  (&define name [&optional stringp] &rest defun-pattern-patterns))
+  (&define name lambda-list lambda-doc &rest defun-pattern-patterns))
 
 ;;;###autoload
-(defmacro defun-pattern (func &rest sub-defs)
-  "Define a function with multiple arglists that are matched.
-Each arglist is paired with a body function.
-
-All arglists are based of the backquoted pcase form.
+(defmacro defun-pattern (func arglist &rest sub-defs)
+  "Define a function which uses pattern matching to execute.
+The macro takes an arglist, which is mostly for documentation
+purposes. The patterns have their own pattern/arglists to match
+against, which then become the arglist for the corresponding
+body function.
 
 If nothing matches, `nil' is returned.
 
@@ -81,21 +94,61 @@ backquoted pcase:
  3. Symbols, when they don't represent variables, but rather
  constants to match against, need to be quoted.
 
-Example:
-(defun-pattern fibonacci
- \"Compute the fibonacci sequence.\"
+Think of the patterns as arglists, except some of the arguments
+are symbols which represent variables which are bound, and others
+are pcase matchers, which can be constants or special matchers
+such as `pred' and others.
+
+Example of a simple defun-pattern:
+(defun-pattern fibonacci (n)
+ \"Compute the fibonacci sequence in a naïve way.\"
  ((0) 0)
  ((1) 1)
- ((n)
- (+ (fibonacci (- n 1))
+ ((n) (+ (fibonacci (- n 1))
     (fibonacci (- n 2)))))
 
-Further examples can be found in defun-pattern-test.el.
+Using an arglist `(&rest args)' will operate as normal in elisp,
+with ARGS being a list. That means patterns matching it have to
+assume it is a list. If you want to dispense with this containing
+list, but still have a variable number of arguments, you can use
+the special arglist (&rest _).
+
+Using a normal arglist of &rest it would be:
+(defun-pattern fibonacci (&rest args)
+ \"Compute the fibonacci sequence in a naïve way.\"
+ (((0)) 0)
+ (((1)) 1)
+ (((n)) (+ (fibonacci (- n 1))
+    (fibonacci (- n 2)))))
+
+Using the special arglist of (&rest _), this becomes:
+(defun-pattern fibonacci (&rest _)
+ \"Compute the fibonacci sequence in a naïve way.\"
+ ((0) 0)
+ ((1) 1)
+ ((n) (+ (fibonacci (- n 1))
+    (fibonacci (- n 2)))))
+
+However, arguments passed in can be used in body methods, and it
+is often useful to have real arguments so that documentation and
+other standard elisp tools can function well.
+
+
+
+Further examples can be found in the file defun-pattern-test.el.
 "
-  (declare (doc-string 2)
+  (declare (doc-string 3)
            (indent 2))
-  `(defun ,func (&rest args)
-     (pcase args
+  `(defun ,func ,arglist
+     (pcase
+         ,@(let* ((normal-args (defun-pattern-normal-args arglist))
+                  (rest-arg (defun-pattern-rest-arg arglist))
+                  (num-args (+ (length normal-args) (length rest-arg))))
+             (cond ((and (= num-args 1) (equal (car rest-arg) '_))
+                    rest-arg)
+                   ((>= num-args 1)
+                    (list (append '(list) normal-args rest-arg)))
+                   (t (error "There must be at least one argument to match against!"))))
        ,@(mapcar (lambda (d)
                    (list (defun-pattern-to-pcase (car d))
                          (if (> (length (cdr d)) 1)
